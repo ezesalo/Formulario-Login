8d1525db723e5f979faee1712c458e52e916e533
ezesalo <ezesalo@gmail.com>
2021-10-08 22:44:47 -0300

    agregado de user class

--- app/src/main/java/com/ejercicios/formulariologin/entities/User.kt
+++ app/src/main/java/com/ejercicios/formulariologin/entities/User.kt
@@ -1,4 +1,57 @@
 package com.ejercicios.formulariologin.entities
 
-class User {
+import android.os.Parcel
+import android.os.Parcelable
+
+class User (nombre: String, apellido: String, email: String, password: String, telefono: String, escliente: Boolean, direcciones:MutableList<String>) :
+    Parcelable {
+
+    var nombre: String
+    var apellido: String
+    var email: String
+    var password: String
+    var telefono: String
+    var escliente: Boolean
+    var direcciones: MutableList<String>
+
+    constructor(parcel: Parcel) : this(
+        TODO("nombre"),
+        TODO("apellido"),
+        TODO("email"),
+        TODO("password"),
+        TODO("telefono"),
+        TODO("escliente"),
+        TODO("direcciones")
+    ) {
+    }
+
+    init {
+        this.nombre = nombre!!
+        this.apellido = apellido!!
+        this.email = email!!
+        this.password = password!!
+        this.telefono = telefono!!
+        this.escliente = true
+        this.direcciones = direcciones!!
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+
 }
